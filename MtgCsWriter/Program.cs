c8ab7c6f8a933696e53fed02b580887976f011c3
a-a-tel <anouaranthonytel@gmail.com>
2026-08-27 12:34:23 +0200

    base code

--- MtgCsWriter/Program.cs
+++ MtgCsWriter/Program.cs
@@ -1,1 +1,25 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace MtgCsWriter;
+
+internal class Program
+{
+    public static void Main()
+    {
+        
+    }
+}
+
+public record SetObject
+{
+    public required string Name;
+    public required string Code;
+    public required string ScryfallUri;
+    public required string ReleasedAt;
+    public required string IconSvgUri;
+}
+
+public record ListObject
+{
+    public required string Object;
+    public required bool HasMore;
+    public required List<SetObject> Data;
+}
